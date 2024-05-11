2bc76513947fa03862c33a7bc9830687a2444c5b
Johnny Zavala <jzavalafgz@gmail.com>
2024-05-10 20:00:54 -0400

    Created first route

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [], // lists other modules this module depends on
+  // tells Nest to use AppController for handling requests directed at the
+  // endpoints defined in it.
   controllers: [AppController],
+  // providers is where you put the services classes, which contain the business logic
+  // that will be injected into controllers.
   providers: [AppService],
 })
 export class AppModule {}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
+import axios from "axios";
 
 function App() {
+  const handleClick = async () => {
+    const repoonse = await axios.get("http://localhost:3002");
+    console.log("RESPONSE", repoonse);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box>Hello</Box>
+      <Button colorScheme="blue" onClick={handleClick}>
+        SUBMIT
+      </Button>
+    </ChakraProvider>
   );
 }
 
