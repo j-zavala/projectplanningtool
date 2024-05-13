cddca8dfebf2132a553d5b54437f7001b0f6d340
Johnny Zavala <jzavalafgz@gmail.com>
2024-05-12 21:15:45 -0400

    feat: create getNames and addName functions

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // the decorator indicates that this method should respond
+  // to POST requests at this path (/name)
+  @Post('/name')
+  async addName(@Body() name: string) {
+    return this.appService.addName(name);
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getNames() {
+    return await this.appService.getNames();
   }
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getNames() {
+    return {};
+  }
+
+  // async because we are going to use database.
+  async addName(name: string) {
+    console.log('--->', name);
+    return name;
   }
 }
