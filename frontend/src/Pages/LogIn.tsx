2ffce0e4b837328e934f94b6a08c76808ae5fdc5
Johnny Zavala <jz@macbook-air.mynetworksettings.com>
2024-06-03 16:29:38 -0400

    feat: added routes via React Router and created Signup UI

---
+++ frontend/src/Pages/LogIn.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+const LogIn = () => {
+  return <Box>Hi</Box>;
+};
+
+export default LogIn;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SignUp from "./Pages/Signup";
+import LogIn from "./Pages/LogIn";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "log-in",
+        element: <LogIn />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+// );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+    <RouterProvider router={router} />
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
