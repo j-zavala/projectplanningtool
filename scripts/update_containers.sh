4370d4861fd23549fb1fb86d4c1e0331c7ab4429
Johnny Zavala <jzavalafgz@gmail.com>
2024-07-04 19:24:31 -0400

    chore: Updated bash script

--- scripts/update_containers.sh
+++ scripts/update_containers.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+ECR_REGISTRY=$1
+ECR_REPOSITORY=$2
+FRONTEND_IMAGE_TAG=$3
+BACKEND_IMAGE_TAG=$4
+
 # Login to ECR
 $(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY)
 
 # Pull the latest images
-docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
-# docker pull $ECR_REGISTRY/$BACKEND_ECR_REPOSITORY:$IMAGE_TAG
+docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$FRONTEND_IMAGE_TAG
+docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$BACKEND_IMAGE_TAG
 
 # Stop and remove old containers
 docker rm -f frontend backend
 
 # Run new containers
-docker run -d -e DATABASE_HOST=$DATABASE_HOST \
-               -e DATABASE_PORT=5432 \
-               -e DATABASE_USER=$DATABASE_USER \
-               -e DATABASE_PASSWORD=$DATABASE_PASSWORD \
-               -e DATABASE_NAME=$DATABASE_NAME \
-               --name backend $ECR_REGISTRY/$BACKEND_ECR_REPOSITORY:$IMAGE_TAG
-
-docker run -d -p 80:80 --name frontend $ECR_REGISTRY/$FRONTEND_ECR_REPOSITORY:$IMAGE_TAG
+docker run -d -p 3000:3000 --name frontend $ECR_REGISTRY/$ECR_REPOSITORY:$FRONTEND_IMAGE_TAG
+docker run -d -p 3005:3005 --name backend $ECR_REGISTRY/$ECR_REPOSITORY:$BACKEND_IMAGE_TAG
